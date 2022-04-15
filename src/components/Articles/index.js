32ad3adda54d7f2c658e7ecf8a006be6b4bd708c
Yanjie L <jasli1999@outlook.com>
2022-04-15 14:37:49 +0200

    made article cards responsive

--- src/components/Articles/index.js
+++ src/components/Articles/index.js
@@ -4,16 +4,16 @@ import { Link } from "react-router-dom";
 
 function Articles(props) {
     return (
-        <div className="flex-holder" style={{ paddingTop: 15 }}>
+        <div className="flex-holder">
             { props.articles.map((article) => {
                 const imageUrl = process.env.REACT_APP_BACKEND_URL +
                                     article.attributes.image.data.attributes.url;
                 return (
                     <Link 
+                        className="article-card"
                         to={`/article/${article.attributes.slug}`} 
                         key={`/article/${article.attributes.slug}`}>
                         <Card
-                            style={{ width: 256, margin: 10 }} 
                             cover={ <img 
                                         style={{ height: 150, objectFit: 'cover'}}
                                         alt={`/article/${article.attributes.slug}`}
