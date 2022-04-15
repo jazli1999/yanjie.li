ab44b19bf3c7e13f03d3725cf7cd644097a89fc6
Yanjie L <jasli1999@outlook.com>
2022-04-15 16:21:17 +0200

    add spin; modify landscape display

--- src/components/Query/index.js
+++ src/components/Query/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import { Spin } from 'antd';
+import { LoadingOutlined } from '@ant-design/icons';
 import { useQuery } from '@apollo/react-hooks';
 
 const Query = ({ children, query, slug }) => {
     const { data, loading, error } = useQuery(query, {
-        variables: {slug: slug}
+        variables: { slug: slug }
     });
 
-    if (loading) return <p>loading...</p>;
+    if (loading) return (
+        <div style={{textAlign: 'center', margin: '15px'}}>
+            <Spin indicator={<LoadingOutlined style={{ fontSize: 24 }} spin />} />
+        </div>
+    );
     if (error) return <p>Error: {JSON.stringify(error)}</p>;
-    return children({data});
+    return children({ data });
 };
 
 export default Query;
