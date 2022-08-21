cbd939140a85853095753aa61f2edfeb50a82213
Yanjie <yanjie.li@tum.de>
2022-08-21 18:20:58 +0200

    fix home width

--- src/components/containers/AboutMe/index.js
+++ src/components/containers/AboutMe/index.js
@@ -8,7 +8,7 @@ function AboutMe() {
         <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexWrap: 'wrap' }}>
             <Personal />
             <div style={{ height: '1px', width: 'calc(100% - 20px)', background: '#e7e4eb' }} />
-            <div style={{ marginTop: '5px' }}>
+            <div style={{ marginTop: '5px', width: '100%' }}>
                 <Home />
             </div>
         </div>
